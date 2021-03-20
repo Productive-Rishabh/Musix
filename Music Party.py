fd003073bb78aec9282d993be7865734d67bbaf7
Rishabh Mehta <77108851+Productive-Rishabh@users.noreply.github.com>
2021-03-20 14:24:44 +0530

    Add files via upload

---
+++ Music Party.py
@@ -0,0 +1,318 @@
+# Lyrics Finder
+print("List of songs\n","\n Love me like you do\n","Do din ki zindagi\n"
+      ,"Mat kar maya Ko ahankar\n","Blank space\n")
+
+x=input("Enter song Name: ")
+
+if x==("Love me like you do"):
+    print("You're the light, you're the night\nYoure the color of my blood\nYou're the cure, you're the pain\n"
+    "You're the only thing I wanna touch\n"
+    "Never knew that it could mean so much, so much\n"
+    "You're the fear, I don't care\n"
+    "'Cause I've never been so high\n"
+    "Follow me through the dark\n"
+    "Let me take you past our satellites\n"
+    "You can see the world you brought to life, to life\n"
+    "So love me like you do, lo-lo-love me like you do\n"
+    "Love me like you do, lo-lo-love me like you do\n"
+    "Touch me like you do, to-to-touch me like you do\n"
+    "What are you waiting for?\n"
+    "Fading in, fading out\n"
+    "On the edge of paradise\n"
+    "Every inch of your skin, is a Holy Grail I've gotta find\n"
+    "Only you can set my heart on fire, on fire\n"
+    "Yeah, I'll let you set the pace\n"
+    "'Cause I'm not thinking straight\n"
+    "My head's spinning around, I can't see clear no more\n"
+    "Oh, what are you waiting for?\n"
+    "Love me like you do, lo-lo-love me like you do\n"
+    "Love me like you do, lo-lo-love me like you do\n"
+    "Touch me like you do, to-to-touch me like you do, oh\n"
+    "What are you, what are you waiting for?\n"
+    "What are you waiting for?\n"
+    "Oh, I'll let you set the pace\n"
+    "'Oh, 'cause I'm not thinking straight\n"
+    "Though my head's spinning around, I can't see clear no more\n"
+    "What are you waiting for?\n"
+    "Love me like you do, lo-lo-love me like you do (Like you do, oh)\n"
+    "Oh, love me like you do\n"
+    "Touch me like you do, to-to-touch me like you do, oh\n"
+    "Oh, what are you waiting for?\n"
+    "Love me like you do, lo-lo-love me like you do (Like you do)\n"
+    "Lo-lo-love me like you do\n"
+    "Touch me like you do, to-to-touch me like you do, oh\n"
+    "What are you, what are you waiting for?\n")
+    
+    
+if x==("Do din ki zindagi"):
+    print("iss jagat saraye Mai musafir rehna do din ka\n"
+    "Qu vritha Kare guman murak iss dhan aur joban ka\n"
+    "Qu vritha Kare guman murak iss dhan aur joban ka\n"
+    "Na he bharosa palka na he bharosa palka yu he\n"
+    "Marr jaGaye yu he marr jayegaa\n"
+    "Do din ki zindagi hai hai do din ka mela\n"
+    "Do din ki zindagi hai do din ka Mela\n"
+    
+    "Kya leke aaya jagat Mai\n"
+    "Kya leke aaya jagat Mai\n"
+    "Kya leke jayega kya leke jayega kya leke jayega\n"
+    "Do din ki zindagi hai do din ka mela\n"
+    "Do din ki zindagi hai do din ka mela\n"
+    "Do din ki zindagi hai do din ka mela\n"
+    "Do din ki zindagi hai do din ka Mela.\n"
+    
+    "Is kaya ka hai bhaag bhaag bin paya nahi jata\n"
+    "Paya nahi jata paya nahi jata\n"
+    "Is kaya ka hai bhaag bhaag bin paya nahi jata\n"
+    
+    "Karm bina naseeb thoda phal khaya nahi jata\n"
+    "Karma bina naseeb thoda phal khaya nahi jata\n"
+    "Kahe satnam jag ye\n"
+    "kahe satnam jag ye jhoota jhamela jhoota jamela\n"
+    "Do din ki zindagi hai do din ka mela\n"
+    "Do din ki zindagi hai do din ka mela\n"
+    
+    "Kya leke aaya jagat Mai kya leke aaya jagat\n"
+    "Mai kya leke jayega kya leke jayega\n"
+    "Do din ki zindagi hai do din ka mela\n"
+    "Do din ki zindagi hai hai do din ka Mela\n")
+    
+    
+if x==("Mat kar maya ko ahankar"):
+    print("मत कर माया को अहंकार\n"
+
+       " मत कर काया को अभिमान\n"
+        
+        "काया गार से कांची\n"
+        
+        
+        
+        "मत कर माया को अहंकार\n"
+        
+        "मत कर काया को अभिमान\n"
+        
+        "काया गार से कांची\n"
+        
+        
+        
+        "हो काया गार से कांची\n"
+        
+        "रे जैसे ओस रा मोती\n"
+        
+        "झोंका पवन का लग जाए\n"
+        
+        "झपका पवन का लग जाए\n"
+        
+        "काया धूल हो जासी\n"
+        
+        "काया तेरी धूल हो जासी\n"
+        
+        
+        
+        "ऐसा सख्त था महाराज\n"
+        
+        "जिनका मुल्कों में राज\n"
+        
+        "जिन घर झूलता हाथी\n"
+        
+        "जिन घर झूलता हाथी\n"
+        
+        "हो जिन घर झूलता हाथी\n"
+        
+        "उन घर दिया ना बाती\n"
+        
+        
+        
+        "झोंका पवन का लग जाए\n"
+        
+        "झपका पवन का लग जाए\n"
+        
+        "काया धूल हो जासी\n"
+        
+        "काया तेरी धूल हो जासी\n"
+        
+        "खूट गया सिन्दड़ा रो तेल\n"
+    
+        "बिखर गया सब निज खेल\n"
+        
+        "बुझ गयी दिया की बाती\n"
+        
+        
+        
+        "हो बुझ गयी दिया की बाती\n"
+        
+        "रे जैसे ओस रा मोती\n"
+        
+        
+        
+        "झोंका पवन का लग जाए\n"
+        
+        "झपका पवन का लग जाए\n"
+        
+        "काया धूल हो जासी\n"
+        
+        "काया तेरी धूल हो जासी\n"
+        
+        
+        
+        "झूठा माई थारो बाप\n"
+        
+        "झूठा सकल परिवार\n"
+        
+        "झूठी कूटता छाती\n"
+        
+        "झूठी कूटता छाती\n"
+        
+        
+        
+        "हो झूठी कूटता छाती\n"
+        
+        "रे जैसे ओस रा मोती\n"
+        
+        
+        
+        "बोल्या भवानी हो नाथ\n"
+        
+        "गुरुजी ने सर पे धरया हाथ\n"
+        
+        "जिनसे मुक्ति मिल जासी\n"
+        
+        "जिनसे मुक्ति मिल जासी\n"
+        
+        
+        
+        "बोल्या भवानी हो नाथ\n"
+        
+        "गुरुजी ने सिर पे धरया हाथ\n"
+        
+        "जिनसे मुक्ति हो जासी\n"
+        
+        "जिनसे मुक्ति हो जासी\n"
+        
+        
+        
+        "हो जिनसे मुक्ति मिल जासी\n"
+        
+        "रे जैसे ओस रा मोती\n"
+        
+        
+        
+        "झोंका… झोंका\n"
+        
+        
+        
+        "मत कर माया को अहंकार\n"
+        
+        "मत कर काया को अभिमान\n"
+        
+        "काया गार से कांची\n"
+        
+        
+        
+        "हो काया गार से कांची\n"
+        
+        "रे जैसे ओस रा मोती\n"
+        
+        
+        
+        "झोंका पवन का लग जाए\n"
+        
+        "झपका पवन का लग जाए\n"
+        
+        "काया धूल हो जासी\n"
+        
+        "काया तेरी धूल हो जासी\n"
+        
+        "काया तेरी धूल हो जासी\n"
+        
+        "काया तेरी धूल हो जासी\n"
+        
+        "काया धूल हो जासी\n"
+    )
+    
+if x==("Blank space"):
+    print("Nice to meet you, where you been?\n"
+    "I could show you incredible things\n"
+    "Magic, madness, heaven, sin\n"
+    "Saw you there and I thought\n"
+    "Oh, my God, look at that face\"\n"
+    "You look like my next mistake"
+    "Love's a game, wanna play? Ay\n"
+    "New money, suit and tie\n"
+    "I can read you like a magazine\n"
+    "Ain't it funny? Rumors fly\n"
+    "And I know you heard about me\n"
+    "So hey, let's be friends\n"
+    "I'm dying to see how this one ends\n"
+    "Grab your passport and my hand\n"
+    "I can make the bad guys good for a weekend\n"
+    "So it's gonna be forever\n"
+    "Or it's gonna go down in flames\n"
+    "You can tell me when it's over, mmh\n"
+    "If the high was worth the pain\n"
+    "Got a long list of ex-lovers\n"
+    "They'll tell you I'm insane\n"
+    "'Cause you know I love the players\n"
+    "And you love the game\n"
+    "'Cause we're young and we're reckless\n"
+    "We'll take this way too far\n"
+    "It'll leave you breathless\n"
+    "Or with a nasty scar\n"
+    "Got a long list of ex-lovers\n"
+    "They'll tell you I'm insane\n"
+    "But I've got a blank space, baby\n"
+    "And I'll write your name\n"
+    "Cherry lips, crystal skies\n"
+    "I could show you incredible things\n"
+    "Stolen kisses, pretty lies\n"
+    "You're the King, baby, I'm your Queen\n"
+    "Find out what you want\n"
+    "Be that girl for a month\n"
+    "Wait, the worst is yet to come, oh, no\n"
+    "Screaming, crying, perfect storms\n"
+    "I can make all the tables turn\n"
+    "Rose garden filled with thorns\n"
+    "Keep you second guessing like\n"
+    "Oh, my God, who is she?\"\n"
+    "I get drunk on jealousy\n"
+    "But you'll come back each time you leave\n"
+    "'Cause, darling, I'm a nightmare dressed like a daydream\n"
+    "So it's gonna be forever\n"
+    "Or it's gonna go down in flames\n"
+    "You can tell me when it's over\n"
+    "If the high was worth the pain\n"
+    "Got a long list of ex-lovers\n"
+    "They'll tell you I'm insane\n"
+    "'Cause you know I love the players\n"
+    "And you love the game\n"
+    "'Cause we're young and we're reckless\n"
+    "We'll take this way too far\n"
+    "It'll leave you breathless\n"
+    "Or with a nasty scar\n"
+    "Got a long list of ex-lovers\n"
+    "They'll tell you I'm insane\n"
+    "But I've got a blank space, baby\n"
+    "And I'll write your name\n"
+    "Boys only want love if it's torture\n"
+    "Don't say I didn't say, I didn't warn ya\n"
+    "Boys only want love if it's torture\n"
+    "Don't say I didn't say, I didn't warn ya\n"
+    "So it's gonna be forever\n"
+    "Or it's gonna go down in flames\n"
+    "You can tell me when it's over\n"
+    "If the high was worth the pain\n"
+    "Got a long list of ex-lovers\n"
+    "They'll tell you I'm insane\n"
+    "'Cause you know I love the players\n"
+    "And you love the game\n"
+    "'Cause we're young and we're reckless\n"
+    "We'll take this way too far\n"
+    "It'll leave you breathless\n"
+    "Or with a nasty scar\n"
+    "Got a long list of ex-lovers\n"
+    "They'll tell you I'm insane\n"
+    "But I've got a blank space, baby\n"
+    "And I'll write your name\n")
+    
+    
+Quit=input("enter any key to exit")
